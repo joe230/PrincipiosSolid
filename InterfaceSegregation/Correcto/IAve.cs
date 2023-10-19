ac2134ef288a7c5bfb6354bc8161d96c60149305
joelcastellvi <joelcm230@gmail.com>
2023-10-19 17:17:55 +0200

    Added Interface Segregation Principle Implementation Closes #7

---
+++ InterfaceSegregation/Correcto/IAve.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrincipiosSolid.InterfaceSegregation.Correcto
+{
+    public interface IAve
+    {
+        void comer();
+    }
+    public interface IAveVoladora
+    {
+        void volar();
+    }
+    public interface IAveNadadora
+    {
+        void nadar();
+    }
+
+    public class Loro : IAve, IAveVoladora
+    {
+        public void comer()
+        {
+            
+        }
+
+        public void volar()
+        {
+            
+        }
+    }
+
+    public class Pinguino : IAve, IAveNadadora
+    {
+        public void comer()
+        {
+            
+        }
+
+        public void nadar()
+        {
+            
+        }
+    }
+}
---
+++ InterfaceSegregation/Incorrecto/IAve.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrincipiosSolid.InterfaceSegregation.Incorrecto
+{
+    public interface IAve
+    {
+        void volar();
+        void comer();
+        void nadar();
+    }
+
+    public class Loro : IAve
+    {
+        public void comer()
+        {
+            
+        }
+
+        public void nadar()
+        {
+            
+        }
+
+        public void volar()
+        {
+            
+        }
+    }
+
+    public class Pinguino : IAve
+    {
+        public void comer()
+        {
+            
+        }
+
+        public void nadar()
+        {
+            
+        }
+
+        public void volar()
+        {
+            
+        }
+    }
+}
