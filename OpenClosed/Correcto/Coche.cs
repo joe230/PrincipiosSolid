0c5f1dba38de1512a8b995d38c6242d9ec2fae23
joelcastellvi <joelcm230@gmail.com>
2023-10-19 13:57:29 +0200

    Added Open/Closed Principle Implementation Closes #3

---
+++ OpenClosed/Correcto/Coche.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrincipiosSolid.OpenClosed.Correcto
+{
+    public abstract class Coche
+    {
+        public abstract int precioMedioCoche();
+
+        public static void main(String[] args)
+        {
+            Coche[] arrayCoches = 
+            {
+                new Renault(),
+                new Audi(),
+                new Mercedes()
+            };
+            imprimirPrecioMedioCoche(arrayCoches);
+        }
+
+        public static void imprimirPrecioMedioCoche(Coche[] arrayCoches)
+        {
+            foreach (var coche in arrayCoches)
+            {
+                Console.WriteLine(coche.precioMedioCoche());
+            }
+        }
+    }
+
+    public class Renault : Coche
+    {
+        public override int precioMedioCoche() { return 18000; }
+    }
+
+    public class Audi : Coche
+    {
+        public override int precioMedioCoche() { return 25000; }
+    }
+
+    public class Mercedes : Coche
+    {
+        public override int precioMedioCoche() { return 27000; }
+    }
+}
